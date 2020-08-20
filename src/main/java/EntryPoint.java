7cd64eedc8a9b0cfb69756ee1684be50ede6a361
Chris Jardine <chris-jardine@outlook.com>
2020-08-20 14:39:48 -0400

    Make changes that occured while trying to get Maven to work on different computer

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -9,7 +9,7 @@ import java.io.IOException;
  * @author Chris Jardine
  * @version 1.0
  */
-public class Driver {
+public class EntryPoint {
 
     public static void main(String[] args) throws IOException {
         LoginWindow window = new LoginWindow();
